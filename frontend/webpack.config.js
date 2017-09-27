062367197a00f798e81015d0df9240181f7e8b0a
Martin Hecher <martin@redsift.io>
2017-09-27 22:57:09 +0100

    add webpack setup for ES6 (wip)

---
+++ frontend/webpack.config.js
@@ -0,0 +1,53 @@
+'use strict';
+
+var webpack = require('webpack'),
+    path = require('path');
+
+var siftRootPath = path.resolve('../');
+
+module.exports = {
+    cache: true,
+    devtool: 'source-map',
+    entry: {
+        controller: ['babel-polyfill', './src/scripts/controller.js'],
+        view: ['babel-polyfill', './src/scripts/view.js'],
+    },
+    output: {
+        filename: '[name].umd-es2015.min.js',
+        path: path.join(__dirname, 'public', 'dist', 'js'),
+    },
+    resolve: {
+        // modules: ['node_modules'],
+        extensions: ['.js', '.jsx']
+    },
+    plugins: [
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('production'),
+        }),
+        new webpack.NoEmitOnErrorsPlugin(),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader?cacheDirectory',
+                    options: {
+                        presets: [
+                            require.resolve('babel-preset-es2015'),
+                            require.resolve('babel-preset-es2017'),
+                        ]
+                    }
+                }
+            },
+        ],
+    },
+    node: {
+        console: true,
+        fs: 'empty',
+        net: 'empty',
+        tls: 'empty',
+    }
+};
